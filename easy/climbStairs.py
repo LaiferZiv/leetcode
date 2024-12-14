e19273e4deab6ad0c960f0b24d11f66a9a43ec32
laiferziv <laiferziv@gmail.com>
2024-12-14 15:17:38 +0200

    problem solved

--- easy/climbStairs.py
+++ easy/climbStairs.py
@@ -1,14 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def dfs(history,num):
-            if num < 0:
-                return 0
-            if num <= 2:
-                history[num] = num
-                return history[num]
-            if history[num] != 0:
-                return history[num]
-            history[num] = dfs(history,num-1) + dfs(history,num-2)
-            return history[num]
-        history = [0] * (n+1)
-        return dfs(history,n)
+        sol,step1,step2 = 0,1,2
+        if n <= 2:
+            return 0 if n < 0 else n
+        for _ in range(n-2):
+            sol = step1 + step2
+            step1 = step2
+            step2 = sol
+        return sol
